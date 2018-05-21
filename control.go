4305f7a707727343fa2b23fe5337976b89279175
Yinjie Chen <28548492+JimmyCYJ@users.noreply.github.com>
2018-05-21 11:39:43 -0700

    Update control.go

--- control.go
+++ control.go
@@ -33,14 +33,20 @@ const (
 	E2eTest   TestType = iota // E2ETEST == 2
 )
 
+// TestTypeString contains test types in string. The order should be in line with enum above.
+var TestTypeString = []string{"unit test", "integration test", "e2e test"}
+
+// TestFileFilters contains filters for parser. The order should be in line with enum above.
+var TestFileFilters = []TestFileFilter{unitTestFileFilter, integTestFileFilter, e2eTestFileFilter}
+
 // TestFileFilter defines filter function signature for parser.
 type TestFileFilter func(os.FileInfo) bool
 
 // unitTestFileFilter filters unit test files.
 func unitTestFileFilter(info os.FileInfo) bool {
 	if !info.IsDir() && strings.HasSuffix(info.Name(), ".go") &&
-		strings.HasSuffix(info.Name(), "_test.go") &&
-		!strings.HasSuffix(info.Name(), "_integ_test.go") {
+			strings.HasSuffix(info.Name(), "_test.go") &&
+			!strings.HasSuffix(info.Name(), "_integ_test.go") {
 		return true
 	}
 	return false
@@ -49,14 +55,17 @@ func unitTestFileFilter(info os.FileInfo) bool {
 // integTestFileFilter filters integration test files.
 func integTestFileFilter(info os.FileInfo) bool {
 	if !info.IsDir() && strings.HasSuffix(info.Name(), ".go") &&
-		strings.HasSuffix(info.Name(), "_integ_test.go") {
+			strings.HasSuffix(info.Name(), "_integ_test.go") {
 		return true
 	}
 	return false
 }
 
-// TestFileFilters contains filters for parser.
-var TestFileFilters = []TestFileFilter{unitTestFileFilter, integTestFileFilter}
-
-// TestTypeString contains test types in string.
-var TestTypeString = []string{"unit test", "integration test"}
+// e2eTestFileFilter filters unit test files.
+func e2eTestFileFilter(info os.FileInfo) bool {
+	if !info.IsDir() && strings.HasSuffix(info.Name(), ".go") &&
+			strings.HasSuffix(info.Name(), "_test.go") {
+		return true
+	}
+	return false
+}
