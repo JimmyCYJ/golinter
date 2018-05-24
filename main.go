825c210bd6c5f3d61edba8c1e1d000bb702b9202
Yinjie Chen <28548492+JimmyCYJ@users.noreply.github.com>
2018-05-24 11:31:41 -0700

    Update main.go

--- main.go
+++ main.go
@@ -47,17 +47,11 @@ func doAllDirs(args []string) []string {
 		// Is it a directory?
 		if fi, err := os.Stat(name); err == nil && fi.IsDir() {
 			err := filepath.Walk(name, func(path string, info os.FileInfo, err error) error {
-				//log.Printf("filepath.Walk path: %q", path)
 				if err != nil {
 					reportErr(fmt.Sprintf("prevent panic by handling failure accessing a path %q: %v\n", path, err))
 					return err
 				}
 				if info.IsDir() {
-					//if strings.HasSuffix(info.Name(), "e2e") {
-					//	for _, r := range doE2eDir(path) {
-					//		reports = append(reports, r.msg)
-					//	}
-					//}
 					if isE2eTestPath(path) {
 						for _, r := range doE2eDir(path) {
 							reports = append(reports, r.msg)
@@ -118,7 +112,7 @@ func testReportsByType(path string, testTypeID TestType) reports {
 }
 
 func doPackage(fs *token.FileSet, pkg *ast.Package, testTypeID TestType) reports {
-	v := newVisitor(fs, testTypeID)
+	v := newReportCollector(fs, testTypeID)
 	switch testTypeID {
 	case UnitTest:
 		scanForbiddenFunctionCallInTest(&v, pkg)
@@ -130,154 +124,6 @@ func doPackage(fs *token.FileSet, pkg *ast.Package, testTypeID TestType) reports
 		log.Printf("Test type is invalid %d", testTypeID)
 	}
 	return v.reports
-}
-
-// scanForbiddenFunctionCallInTest scans tests and checks forbidden function call.
-func scanForbiddenFunctionCallInTest(v *visitor, pkg *ast.Package) {
-	for _, file := range pkg.Files {
-		ast.Walk(v, file)
-	}
-}
-
-// scanMandatoryFunctionCallInTest scans tests and checks if mandatory function call is placed at
-// the beginning of each test.
-func scanMandatoryFunctionCallInTest(v *visitor, pkg *ast.Package) {
-	for _, file := range pkg.Files {
-		testFuncs := []*ast.FuncDecl{}
-		for _, d := range file.Decls {
-			if fn, isFn := d.(*ast.FuncDecl); isFn {
-				testFuncs = append(testFuncs, fn)
-			}
-		}
-		// Checks each test function named TestXxx.
-		for _, function := range testFuncs {
-			// log.Printf("-- function %s", function.Name.String())
-			if !hasMandatoryCall(function.Body.List[0]) {
-				v.reports = append(v.reports, v.missingMandatoryCallReport(file.Pos(), function))
-			}
-		}
-	}
-}
-
-// hasMandatoryCall examines the mandatory function call in a function of the form TestXxx.
-// Currently we check the following calls.
-// case 1:
-//if testing.Short() {
-//	t.Skip("skipping uint test in short mode.")
-//}
-// case 2:
-// if !testing.Short() {
-// ...
-// }
-func hasMandatoryCall(stmt ast.Stmt) bool {
-	hasShortAtTop := false
-	hasSkipAtTop := false
-	if ifStmt, ok := stmt.(*ast.IfStmt); ok {
-		if call, ok := ifStmt.Cond.(*ast.CallExpr); ok {
-			if fun, ok := call.Fun.(*ast.SelectorExpr); ok {
-				// funcName := fun.X.(*ast.Ident).String() + "." + fun.Sel.String() + "()"
-				if astid, ok := fun.X.(*ast.Ident); ok {
-					if astid.String() == "testing" && fun.Sel.String() == "Short" {
-						hasShortAtTop = true
-					}
-				}
-			}
-			if len(ifStmt.Body.List) > 0 {
-				if exprStmt, ok := ifStmt.Body.List[0].(*ast.ExprStmt); ok {
-					if call, ok := exprStmt.X.(*ast.CallExpr); ok {
-						if fun, ok := call.Fun.(*ast.SelectorExpr); ok {
-							// funcName := fun.X.(*ast.Ident).String() + "." + fun.Sel.String() + "()"
-							if astid, ok := fun.X.(*ast.Ident); ok {
-								if astid.String() == "t" && fun.Sel.String() == "Skip" {
-									hasSkipAtTop = true
-								}
-							}
-						}
-					}
-				}
-			}
-		} else if uExpr, ok := ifStmt.Cond.(*ast.UnaryExpr); ok {
-			if call, ok := uExpr.X.(*ast.CallExpr); ok && uExpr.Op == token.NOT {
-				if fun, ok := call.Fun.(*ast.SelectorExpr); ok {
-					if astid, ok := fun.X.(*ast.Ident); ok {
-						if astid.String() == "testing" && fun.Sel.String() == "Short" {
-							hasShortAtTop = true
-							hasSkipAtTop = true
-						}
-					}
-				}
-			}
-		}
-	}
-	return hasShortAtTop && hasSkipAtTop
-}
-
-func (v *visitor) missingMandatoryCallReport(pos token.Pos, testFunc *ast.FuncDecl) report {
-	return report{
-		pos,
-		fmt.Sprintf("%v:%v:%v:%s %s %s",
-			v.fs.Position(pos).Filename,
-			v.fs.Position(testFunc.Pos()).Line,
-			v.fs.Position(testFunc.Pos()).Column,
-			"Missing testing.Short() call and t.Skip() call at the beginning of",
-			TestTypeString[v.typeID], testFunc.Name.String()),
-	}
-}
-
-func newVisitor(fs *token.FileSet, testTypeID TestType) visitor {
-	return visitor{
-		fs:     fs,
-		typeID: testTypeID,
-	}
-}
-
-type visitor struct {
-	reports reports
-	fs      *token.FileSet
-	typeID  TestType
-}
-
-/*
-Validates the following for _test.go files
-1. Disallow use of time.Sleep() in _test.go
-2. Disallow use of testing.Short() in _test.go
-*/
-func (v *visitor) Visit(node ast.Node) ast.Visitor {
-	if node == nil {
-		return nil
-	}
-
-	ce, ok := node.(*ast.CallExpr)
-	if ok {
-		for _, utci := range UnitTestCheckList {
-			if isInvalidCall(ce.Fun, utci) {
-				v.reports = append(v.reports, v.invalidCallReport(ce.Pos(), utci))
-			}
-		}
-	}
-
-	return v
-}
-
-func isInvalidCall(expr ast.Expr, utci UnitTestCheckItem) bool {
-	sel, ok := expr.(*ast.SelectorExpr)
-	return ok && isIdent(sel.X, utci.pkgName) && isIdent(sel.Sel, utci.mName)
-}
-
-func isIdent(expr ast.Expr, ident string) bool {
-	id, ok := expr.(*ast.Ident)
-	return ok && id.Name == ident
-}
-
-func (v *visitor) invalidCallReport(pos token.Pos, utci UnitTestCheckItem) report {
-	return report{
-		pos,
-		fmt.Sprintf("%v:%v:%v:%s",
-			v.fs.Position(pos).Filename,
-			v.fs.Position(pos).Line,
-			v.fs.Position(pos).Column,
-			"invalid "+utci.pkgName+"."+utci.mName+"() call in "+TestTypeString[v.typeID]),
-	}
 }
 
 type report struct {
@@ -294,4 +140,62 @@ func (l reports) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
 func reportErr(msg string) {
 	fmt.Fprintln(os.Stderr, msg)
 	exitCode = 2
+}
+
+func newReportCollector(fs *token.FileSet, testTypeID TestType) reportCollector {
+	return reportCollector{
+		fs:     fs,
+		typeID: testTypeID,
+	}
+}
+
+type reportCollector struct {
+	reports reports
+	fs      *token.FileSet
+	typeID  TestType
+}
+
+/*
+Validates the following for _test.go files
+1. Disallow use of time.Sleep() in _test.go
+2. Disallow use of testing.Short() in _test.go
+*/
+func (v *reportCollector) Visit(node ast.Node) ast.Visitor {
+	if node == nil {
+		return nil
+	}
+
+	ce, ok := node.(*ast.CallExpr)
+	if ok {
+		for _, utci := range UnitTestCheckList {
+			if isForbiddenCall(ce.Fun, utci) {
+				v.reports = append(v.reports, v.forbiddenCallReport(ce.Pos(), utci))
+			}
+		}
+	}
+
+	return v
+}
+
+func (v *reportCollector) forbiddenCallReport(pos token.Pos, utci UnitTestCheckItem) report {
+	return report{
+		pos,
+		fmt.Sprintf("%v:%v:%v:%s",
+			v.fs.Position(pos).Filename,
+			v.fs.Position(pos).Line,
+			v.fs.Position(pos).Column,
+			"invalid "+utci.pkgName+"."+utci.mName+"() call in "+TestTypeString[v.typeID]),
+	}
+}
+
+func (v *reportCollector) MissingMandatoryCallReport(pos token.Pos, testFunc *ast.FuncDecl) report {
+	return report{
+		pos,
+		fmt.Sprintf("%v:%v:%v:%s %s %s",
+			v.fs.Position(pos).Filename,
+			v.fs.Position(testFunc.Pos()).Line,
+			v.fs.Position(testFunc.Pos()).Column,
+			"Missing testing.Short() call and t.Skip() call at the beginning of",
+			TestTypeString[v.typeID], testFunc.Name.String()),
+	}
 }
