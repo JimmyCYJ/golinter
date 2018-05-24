a2b8c9f83851745f8ccdb2bf7dd88c54ee8e52ef
Yinjie Chen <28548492+JimmyCYJ@users.noreply.github.com>
2018-05-24 11:32:13 -0700

    Update and rename control.go to golinter_setup.go

--- golinter_setup.go
+++ golinter_setup.go
@@ -5,24 +5,6 @@ import (
 	"strings"
 )
 
-// FuncCheckItem defines the information of a function that needs to check.
-type UnitTestCheckItem struct {
-	pkgName string // package name of the function call.
-	mName   string // method name of the function call.
-}
-
-// UnitTestCheckList lists all functions calls should not exist in unit test files.
-var UnitTestCheckList = []UnitTestCheckItem{
-	UnitTestCheckItem{
-		"time",
-		"Sleep",
-	},
-	UnitTestCheckItem{
-		"testing",
-		"Short",
-	},
-}
-
 // TestType is type ID of tests
 type TestType int
 
@@ -45,7 +27,7 @@ type TestFileFilter func(os.FileInfo) bool
 // unitTestFileFilter filters unit test files.
 func unitTestFileFilter(info os.FileInfo) bool {
 	if strings.HasSuffix(info.Name(), "_test.go") &&
-	   !strings.HasSuffix(info.Name(), "_integ_test.go") {
+			!strings.HasSuffix(info.Name(), "_integ_test.go") {
 		return true
 	}
 	return false
