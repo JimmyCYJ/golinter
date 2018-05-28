cc17638512f5940e6d1c31172adcb21746822b58
Yinjie Chen <28548492+JimmyCYJ@users.noreply.github.com>
2018-05-28 12:09:19 -0700

    Update function_call_scanner.go

--- function_call_scanner.go
+++ function_call_scanner.go
@@ -1,65 +1,178 @@
-// Checks unit test files and look for forbidden function call in each test function.
-// And checks if integration test files and e2e test files have mandatory function call
-// at the beginning of each test function.
+// Copyright 2018 Istio Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package main
 
 import (
+	"fmt"
 	"go/ast"
+	"go/parser"
 	"go/token"
+	"log"
 )
 
-// FuncCheckItem defines the information of a function that needs to check.
-type UnitTestCheckItem struct {
-	pkgName string // package name of the function call.
-	mName   string // method name of the function call.
+// LintReport stores report after processing a file.
+type LintReport struct {
+	pos token.Pos
+	msg string
 }
 
-// UnitTestCheckList lists all functions calls should not exist in unit test files.
-var UnitTestCheckList = []UnitTestCheckItem{
-	UnitTestCheckItem{
-		"time",
-		"Sleep",
-	},
-	UnitTestCheckItem{
-		"testing",
-		"Short",
-	},
+// LintReports stores a group of lint processing reports.
+type LintReports []LintReport
+
+type forbiddenFunctionList struct {
+	fInfos []string
+	goRt   bool // Whether to check goroutine
 }
 
-// scanForbiddenFunctionCallInTest scans tests and checks forbidden function call.
-func scanForbiddenFunctionCallInTest(v *reportCollector, pkg *ast.Package) {
-	for _, file := range pkg.Files {
-		ast.Walk(v, file)
+func newForbiddenFunctionList() forbiddenFunctionList {
+	ffl := forbiddenFunctionList{
+		fInfos: forbiddenFunctionCalls,
+		goRt:   true,
+	}
+	return ffl
+}
+
+// Linter applies linting rules to a file.
+type Linter struct {
+	fpath   string                 // file path that takes linting process
+	lreport LintReports            // report for linting process
+	tType   TestType               // test type of file path
+	ffl     *forbiddenFunctionList // pointer pointing to list of forbidden functions
+	fs      *token.FileSet
+}
+
+func newLinter(fileP string, t TestType, ffnl *forbiddenFunctionList) Linter {
+	return Linter{
+		fpath: fileP,
+		tType: t,
+		ffl:   ffnl,
+		fs:    token.NewFileSet(),
 	}
 }
 
-func isForbiddenCall(expr ast.Expr, utci UnitTestCheckItem) bool {
-	sel, ok := expr.(*ast.SelectorExpr)
-	return ok && isIdent(sel.X, utci.pkgName) && isIdent(sel.Sel, utci.mName)
+// LReport returns report from Linter
+func (lt *Linter) LReport() LintReports {
+	return lt.lreport
 }
 
-func isIdent(expr ast.Expr, ident string) bool {
+// Fs returns fs from Linter
+func (lt *Linter) Fs() *token.FileSet {
+	return lt.fs
+}
+
+// Run applies linting rule to file in fpath.
+func (lt *Linter) Run() {
+	astFile, err := parser.ParseFile(lt.Fs(), lt.fpath, nil, parser.Mode(0))
+	if err != nil {
+		lt.lreport = append(lt.lreport, LintReport{token.NoPos, fmt.Sprintf("%v", err)})
+		return
+	}
+	switch lt.tType {
+	case UnitTest:
+		lt.scanForbiddenFunctionCallInTest(astFile)
+	case IntegTest:
+		lt.scanMandatoryFunctionCallInTest(astFile)
+	case E2eTest:
+		lt.scanMandatoryFunctionCallInTest(astFile)
+	default:
+		log.Printf("Test type is invalid %d", lt.tType)
+	}
+}
+
+func (lt *Linter) scanForbiddenFunctionCallInTest(af *ast.File) {
+	ast.Walk(lt, af)
+}
+
+// Visit checks each function call and report if a forbidden function call is detected.
+func (lt *Linter) Visit(node ast.Node) ast.Visitor {
+	if node == nil {
+		return nil
+	}
+
+	ce, ok := node.(*ast.CallExpr)
+	if ok {
+		// Check function call against forbidden function list and update report.
+		for _, finfo := range lt.ffl.fInfos {
+			if lt.isForbiddenCall(ce.Fun, finfo) {
+				lt.updateLintReport(finfo, ce.Pos(), nil)
+			}
+		}
+	}
+	// Check goroutine call.
+	gs, ok := node.(*ast.GoStmt)
+	if ok && lt.ffl.goRt {
+		lt.updateLintReport("goroutine", gs.Pos(), nil)
+	}
+
+	return lt
+}
+
+// updateLintReport updates report catching a forbidden function call finfo or a missing
+// mandatory function finfo.
+func (lt *Linter) updateLintReport(finfo string, pos token.Pos, testFunc *ast.FuncDecl) {
+	var reportMsg string
+	if lt.tType == UnitTest {
+		reportMsg = fmt.Sprintf("%v:%v:%v:%s",
+			lt.fs.Position(pos).Filename,
+			lt.fs.Position(pos).Line,
+			lt.fs.Position(pos).Column,
+			"invalid "+finfo+"() call in "+TestTypeToString[lt.tType])
+	} else {
+		reportMsg = fmt.Sprintf("%v:%v:%v:%s %s %s",
+			lt.fs.Position(pos).Filename,
+			lt.fs.Position(testFunc.Pos()).Line,
+			lt.fs.Position(testFunc.Pos()).Column,
+			"Missing testing.Short() call and t.Skip() call at the beginning of",
+			TestTypeToString[lt.tType], testFunc.Name.String())
+	}
+	newRpt := LintReport{
+		pos,
+		reportMsg,
+	}
+	lt.lreport = append(lt.lreport, newRpt)
+}
+
+// isForbiddenCall returns true if expr matches forbidden function fn.
+func (lt *Linter) isForbiddenCall(expr ast.Expr, fn string) bool {
+	if sel, ok := expr.(*ast.SelectorExpr); ok {
+		pkg, pkgok := sel.X.(*ast.Ident)
+		if pkgok {
+			fcall := pkg.Name + "." + sel.Sel.Name
+			return fcall == fn
+		}
+	}
+	return false
+}
+
+func (lt *Linter) isIdent(expr ast.Expr, ident string) bool {
 	id, ok := expr.(*ast.Ident)
 	return ok && id.Name == ident
 }
 
-// scanMandatoryFunctionCallInTest scans tests and checks if mandatory function call is placed at
-// the beginning of each test.
-func scanMandatoryFunctionCallInTest(v *reportCollector, pkg *ast.Package) {
-	for _, file := range pkg.Files {
-		testFuncs := []*ast.FuncDecl{}
-		for _, d := range file.Decls {
-			if fn, isFn := d.(*ast.FuncDecl); isFn {
-				testFuncs = append(testFuncs, fn)
-			}
+func (lt *Linter) scanMandatoryFunctionCallInTest(af *ast.File) {
+	testFuncs := []*ast.FuncDecl{}
+	for _, d := range af.Decls {
+		if fn, isFn := d.(*ast.FuncDecl); isFn {
+			testFuncs = append(testFuncs, fn)
 		}
-		// Checks each test function named TestXxx.
-		for _, function := range testFuncs {
-			// log.Printf("-- function %s", function.Name.String())
-			if !hasMandatoryCall(function.Body.List) {
-				v.reports = append(v.reports, v.MissingMandatoryCallReport(file.Pos(), function))
-			}
+	}
+	// Checks each test function named TestXxx.
+	for _, function := range testFuncs {
+		// log.Printf("-- function %s", function.Name.String())
+		if !lt.hasMandatoryCall(function.Body.List) {
+			lt.updateLintReport("", af.Pos(), function)
 		}
 	}
 }
@@ -79,7 +192,7 @@ func scanMandatoryFunctionCallInTest(v *reportCollector, pkg *ast.Package) {
 // 	...
 // 	}
 // }
-func hasMandatoryCall(stmts []ast.Stmt) bool {
+func (lt *Linter) hasMandatoryCall(stmts []ast.Stmt) bool {
 	if len(stmts) == 0 {
 		return false
 	} else if len(stmts) == 1 {
